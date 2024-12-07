110b60002517f46bf6d1f527280ca20ee0266316
Bo Xu <bo@boxu.science>
2024-12-06 23:13:18 -0700

    day 7

--- day 7.py
+++ day 7.py
@@ -1,73 +1,81 @@
-#function to do calculation
+# function to do calculation
 def calculation(nums, operators):
     result = nums[0]
-    for i in range (0, len(operators)):
+    for i in range(0, len(operators)):
         if operators[i] == 1:
-            result += nums[i+1]
+            result += nums[i + 1]
         if operators[i] == 0:
-            result *= nums[i+1]
-
+            result *= nums[i + 1]
     return result
 
-#function to sweep the possible operators
+
+# function to sweep the possible operators
 def sweep_operator(nums):
     result_list = []
     N = len(nums) - 1
-    for i in range (0, 2**N):
+    for i in range(0, 2**N):
         operators = []
-        for j in range (0, N):
-            op_bit, i = divmod(i, (2**(N-j-1)))
+        for j in range(0, N):
+            op_bit, i = divmod(i, (2 ** (N - j - 1)))
             operators.append(op_bit)
-        #print(operators)
+        # print(operators)
         result = calculation(nums, operators)
         result_list.append(result)
     return result_list
 
-#function to do calculation
+
+# function to do calculation
 def calculation_v2(nums, operators):
     result = nums[0]
-    for i in range (0, len(operators)):
+    for i in range(0, len(operators)):
         if operators[i] == 1:
-            result += nums[i+1]
+            result += nums[i + 1]
         if operators[i] == 0:
-            result *= nums[i+1]
+            result *= nums[i + 1]
         if operators[i] == 2:
-            new_str = str(result) + str(nums[i+1])
+            new_str = str(result) + str(nums[i + 1])
             result = int(new_str)
     return result
 
-#function to sweep the possible operators
+
+# function to sweep the possible operators
 def sweep_operator_v2(nums):
     result_list = []
     N = len(nums) - 1
-    for i in range (0, 3**N):
+    for i in range(0, 3**N):
         operators = []
-        for j in range (0, N):
-            op_bit, i = divmod(i, (3**(N-j-1)))
+        for j in range(0, N):
+            op_bit, i = divmod(i, (3 ** (N - j - 1)))
             operators.append(op_bit)
-        #print(operators)
+        # print(operators)
         result = calculation_v2(nums, operators)
         result_list.append(result)
     return result_list
 
-test_value = []
-num_equation = []
 
-file_path = "/Users/boxu/Dev/aoc 2024/day 7_input.txt"
-with open(file_path, 'r') as file:
-    for line in file:
-        a, b = line.split(":")
-        test_value.append(int(a.strip()))
-        b_str = b.strip().split()
-        b_num = [int(x) for x in b_str]
-        num_equation.append(b_num)
+def main():
+    test_value = []
+    num_equation = []
 
-good_value = []
-for i in range (0, len(test_value)):
-    value = test_value[i]
-    nums = num_equation[i]
-    result_list = sweep_operator_v2(nums)
-    if value in result_list:
-        good_value.append(value)
+    file_path = "/Users/boxu/Dev/aoc 2024/day 7_input.txt"
+    with open(file_path, "r") as file:
+        for line in file:
+            a, b = line.split(":")
+            test_value.append(int(a.strip()))
+            b_str = b.strip().split()
+            b_num = [int(x) for x in b_str]
+            num_equation.append(b_num)
 
-print(sum(good_value))
+    good_value = []
+    for i in range(0, len(test_value)):
+        value = test_value[i]
+        nums = num_equation[i]
+        result_list = sweep_operator(nums)
+        if value in result_list:
+            good_value.append(value)
+
+    print(sum(good_value))
+
+
+if __name__ == "__main__":
+    main()
